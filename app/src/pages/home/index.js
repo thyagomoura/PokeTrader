b58b89eb572b8175bef4b4eddb4b0770704d1a24
thyagomoura <thyagohenriquemoura@gmail.com>
2022-03-14 18:47:34 -0300

    home page add right

--- app/src/pages/home/index.js
+++ app/src/pages/home/index.js
@@ -1,7 +1,31 @@
+import '../../styles/home.css';
+import Charmander from '../../assets/charmander.png';
+
 function homePage(){
     return(
-        <div>
-            Rule Page
+        <div className="HomeGlobalContainer">
+            
+            <div className="HomeContentBox">
+                <div className="HomeImg">
+                    <img alt="Charmander Pokemon" src={Charmander}/>
+                </div>
+                <div className="Message">
+                    <p>Olá, seja Bem Vindo ao Poke Trade!!</p> 
+                    <p>
+                        Funciona assim:<br/>
+                        1. O primeiro jogador vai ofertar entre 1 e 6 pokemons e o segundo jogador tambem.<br/><br/>
+                        Qualquer combinação é valida!<br/><br/>
+
+                        2. Uma troca é considerada justa, se a soma das bases de experiencia dos pokemons de cada lado da troca é próxima uma da outra.
+                    </p>
+                    Ok, vamos lá então??
+                </div>
+                <div className="HomeButtonBox">
+                    <button type="button" className="HomeButton">
+                        START
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
